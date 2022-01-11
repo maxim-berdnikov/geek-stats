a0a4b715b529651e165bd2236e5d3341261da883
Maxim Berdnikov <m.radik25@gmail.com>
2022-01-11 21:22:12 +0300

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MonthList } from "./MonthList";
+import { ChartElement } from "./ChartElement";
+import s from "./style.module.css";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.app}>
+      <h1>Monthly Stats</h1>
+      <div>
+        <ChartElement />
+        <MonthList />
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/ChartElement.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
+import { DB } from "./DB";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top"
+    }
+    // title: {
+    //   display: true,
+    //   text: "Chart.js Line Chart"
+    // }
+  }
+};
+
+const colors = {
+  comics: {
+    line: "#F5B041",
+    bg: "#FAD7A0"
+  },
+  tv_shows: {
+    line: "#52BE80",
+    bg: "#ABEBC6"
+  },
+  games: {
+    line: "#EC7063",
+    bg: "#F5B7B1"
+  },
+  movies: {
+    line: "#5DADE2",
+    bg: "#AED6F1"
+  },
+  books: {
+    line: "#EC7063",
+    bg: "#F5B7B1"
+  }
+};
+
+const showedData = [
+  {
+    label: "Comics",
+    data: DB.map((month) => month.comics.length),
+    borderColor: colors.comics.line,
+    backgroundColor: colors.comics.bg
+  },
+  {
+    label: "TV Shows",
+    data: DB.map((month) => month.tv_shows.length),
+    borderColor: colors.tv_shows.line,
+    backgroundColor: colors.tv_shows.bg
+  },
+  {
+    label: "Games",
+    data: DB.map((month) => month.games.length),
+    borderColor: colors.games.line,
+    backgroundColor: colors.games.bg
+  },
+  {
+    label: "Movies",
+    data: DB.map((month) => month.movies.length),
+    borderColor: colors.movies.line,
+    backgroundColor: colors.movies.bg
+  }
+];
+
+const labels = DB.map((month) => month.month);
+
+export const data = {
+  labels,
+  datasets: showedData
+};
+
+export function ChartElement() {
+  /* @ts-ignore */
+  return <Line options={options} data={data} />;
+}
