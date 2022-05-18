a1ba6784e6fb856bc12a7931307b7774aa4e92f6
Maxim Berdnikov <m.radik25@gmail.com>
2022-05-18 22:15:04 +0300

    update db

--- src/Components/ChartElement.tsx
+++ src/Components/ChartElement.tsx
@@ -7,7 +7,7 @@ import {
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 } from "chart.js";
 import { DB } from "./DB";
 import { Line } from "react-chartjs-2";
@@ -26,73 +26,78 @@ const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: "top"
-    }
+      position: "top",
+    },
     // title: {
     //   display: true,
     //   text: "Chart.js Line Chart"
     // }
-  }
+  },
 };
 
 const colors = {
   comics: {
     line: "#F5B041",
-    bg: "#FAD7A0"
+    bg: "#FAD7A0",
   },
   tv_shows: {
     line: "#52BE80",
-    bg: "#ABEBC6"
+    bg: "#ABEBC6",
   },
   games: {
     line: "#EC7063",
-    bg: "#F5B7B1"
+    bg: "#F5B7B1",
   },
   movies: {
     line: "#5DADE2",
-    bg: "#AED6F1"
+    bg: "#AED6F1",
   },
   books: {
     line: "#EC7063",
-    bg: "#F5B7B1"
-  }
+    bg: "#F5B7B1",
+  },
 };
 
 const showedData = [
   {
     label: "Comics",
-    data: DB.map((month) => month.comics.length),
+    data: DB.map((month) => month.content.comics.length),
     borderColor: colors.comics.line,
-    backgroundColor: colors.comics.bg
+    backgroundColor: colors.comics.bg,
   },
   {
     label: "TV Shows",
-    data: DB.map((month) => month.tv_shows.length),
+    data: DB.map((month) => month.content.tv_shows.length),
     borderColor: colors.tv_shows.line,
-    backgroundColor: colors.tv_shows.bg
+    backgroundColor: colors.tv_shows.bg,
   },
   {
     label: "Games",
-    data: DB.map((month) => month.games.length),
+    data: DB.map((month) => month.content.games.length),
     borderColor: colors.games.line,
-    backgroundColor: colors.games.bg
+    backgroundColor: colors.games.bg,
   },
   {
     label: "Movies",
-    data: DB.map((month) => month.movies.length),
+    data: DB.map((month) => month.content.movies.length),
     borderColor: colors.movies.line,
-    backgroundColor: colors.movies.bg
-  }
+    backgroundColor: colors.movies.bg,
+  },
 ];
 
 const labels = DB.map((month) => month.month);
 
 export const data = {
   labels,
-  datasets: showedData
+  datasets: showedData,
 };
 
 export function ChartElement() {
-  /* @ts-ignore */
-  return <Line options={options} data={data} />;
+ 
+  return (
+    <div className="canvas-container">
+      {/* @ts-ignore */}
+      <Line options={options} data={data} />
+    </div>
+  );
 }
